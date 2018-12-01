11ea4855e705363ee044edef8dc0c6e42dc393ff
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2018-12-01 15:01:45 -0200

    Up-to-date 01/12

--- MyMVC/MyMVC/Controllers/Cadastro/CadProdutoController.cs
+++ MyMVC/MyMVC/Controllers/Cadastro/CadProdutoController.cs
@@ -52,6 +52,21 @@ namespace MyMVC.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult RecuperarQuantidadeEstoqueProduto(int id)
+        {
+            var model = ProdutoModel.RecuperarPeloId(id);
+            if (model != null)
+            {
+                return Json(new { OK = true, Result = model.QuantEstoque });
+            }
+            else
+            {
+                return Json(new { OK = false });
+            }
+        }
+
+        [HttpPost]
         [Authorize(Roles = "Gerente,Administrativo")]
         [ValidateAntiForgeryToken]
         public JsonResult ExcluirProduto(int id)
